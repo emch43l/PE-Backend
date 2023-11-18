c4878ecf30b60dea84ad5da7f04157f9c0173ac2
emch43l <michalgood10@gmail.com>
2023-11-18 21:24:17 +0100

    Added one untracked file

---
+++ Infrastructure/DB/Configuration/FileEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Model;
+using Infrastructure.Identity.Entity.Join;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DB.Configuration;
+
+public class FileEntityConfiguration : IEntityTypeConfiguration<FileEntity<int>>
+{
+    public void Configure(EntityTypeBuilder<FileEntity<int>> builder)
+    {
+        builder.HasKey(file => file.Id);
+        builder
+            .HasOne(file => file.Post)
+            .WithMany(post => post.Files)
+            .HasForeignKey("PostId");
+        builder
+            .HasOne(file => file.Comment)
+            .WithOne(comment => comment.File)
+            .HasForeignKey("CommentId");
+        builder
+            .HasMany(file => file.Albums)
+            .WithMany(album => album.Files)
+            .UsingEntity<AlbumFileJoin<int>>(
+                albumFile => albumFile.ToTable("AlbumsFiles")
+            );
+        builder.ToTable("Files");
+    }
+}
